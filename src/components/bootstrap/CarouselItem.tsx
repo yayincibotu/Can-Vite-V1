dbe6ba66f8fb557b438ebf326a0b3a8b69146310
YayinciBotu <73336833+yayincibotu@users.noreply.github.com>
2025-01-09 09:08:49 +0200

    Main

---
+++ src/components/bootstrap/CarouselItem.tsx
@@ -0,0 +1,135 @@
+import React, { Component, ReactNode } from 'react';
+import classNames from 'classnames';
+import { Transition } from 'react-transition-group';
+import { TransitionStatuses, TransitionTimeouts } from './utils';
+
+type TCarouselItemProps = {
+	tag?: string;
+	in?: boolean;
+	children?: ReactNode;
+	slide?: boolean;
+	className?: string;
+	isFluid?: boolean;
+	onEnter(...args: unknown[]): unknown;
+	onEntering(...args: unknown[]): unknown;
+	onExit(...args: unknown[]): unknown;
+	onExiting(...args: unknown[]): unknown;
+	onExited(...args: unknown[]): unknown;
+};
+type TCarouselItemState = {
+	startAnimation: boolean;
+};
+class CarouselItem extends Component<TCarouselItemProps, TCarouselItemState> {
+	constructor(props: TCarouselItemProps | Readonly<TCarouselItemProps>) {
+		super(props);
+
+		this.state = {
+			startAnimation: false,
+		};
+
+		this.onEnter = this.onEnter.bind(this);
+		this.onEntering = this.onEntering.bind(this);
+		this.onExit = this.onExit.bind(this);
+		this.onExiting = this.onExiting.bind(this);
+		this.onExited = this.onExited.bind(this);
+	}
+
+	onEnter(node: any, isAppearing: any) {
+		this.setState({ startAnimation: false });
+
+		this.props.onEnter(node, isAppearing);
+	}
+
+	onEntering(node: { offsetHeight: any }, isAppearing: any) {
+		// getting this variable triggers a reflow
+		const { offsetHeight } = node;
+		this.setState({ startAnimation: true });
+
+		this.props.onEntering(node, isAppearing);
+		return offsetHeight;
+	}
+
+	onExit(node: any) {
+		this.setState({ startAnimation: false });
+
+		this.props.onExit(node);
+	}
+
+	onExiting(node: { dispatchEvent: (arg0: CustomEvent<unknown>) => void }) {
+		this.setState({ startAnimation: true });
+		node.dispatchEvent(new CustomEvent('slide.bs.carousel'));
+
+		this.props.onExiting(node);
+	}
+
+	onExited(node: { dispatchEvent: (arg0: CustomEvent<unknown>) => void }) {
+		node.dispatchEvent(new CustomEvent('slid.bs.carousel'));
+
+		this.props.onExited(node);
+	}
+
+	render() {
+		const {
+			in: isIn,
+			children,
+			slide,
+			tag: Tag,
+			className,
+			isFluid,
+			...transitionProps
+		} = this.props;
+
+		return (
+			// @ts-ignore
+			<Transition
+				// eslint-disable-next-line react/jsx-props-no-spreading
+				{...transitionProps}
+				enter={slide}
+				exit={slide}
+				in={isIn}
+				onEnter={this.onEnter}
+				onEntering={this.onEntering}
+				onExit={this.onExit}
+				onExiting={this.onExiting}
+				onExited={this.onExited}>
+				{(status) => {
+					// @ts-ignore
+					const { direction } = this.context;
+					const isActive =
+						status === TransitionStatuses.ENTERED ||
+						status === TransitionStatuses.EXITING;
+					const directionClassName =
+						(status === TransitionStatuses.ENTERING ||
+							status === TransitionStatuses.EXITING) &&
+						this.state.startAnimation &&
+						(direction === 'end' ? 'carousel-item-start' : 'carousel-item-end');
+					const orderClassName =
+						status === TransitionStatuses.ENTERING &&
+						(direction === 'end' ? 'carousel-item-next' : 'carousel-item-prev');
+					const itemClasses = classNames(
+						className,
+						'carousel-item',
+						isActive && 'active',
+						directionClassName,
+						orderClassName,
+						{ 'h-100': isFluid },
+					);
+
+					// @ts-ignore
+					return <Tag className={itemClasses}>{children}</Tag>;
+				}}
+			</Transition>
+		);
+	}
+}
+// @ts-ignore
+CarouselItem.defaultProps = {
+	// @ts-ignore
+	...Transition.defaultProps,
+	tag: 'div',
+	timeout: TransitionTimeouts.Carousel,
+	slide: true,
+	isFluid: false,
+};
+
+export default CarouselItem;
---
+++ src/layout/PageWrapper/PageWrapper.tsx
@@ -0,0 +1,52 @@
+import React, { forwardRef, ReactElement, useContext, useEffect, useLayoutEffect } from 'react';
+import classNames from 'classnames';
+import { useNavigate } from 'react-router-dom';
+import { ISubHeaderProps } from '../SubHeader/SubHeader';
+import { IPageProps } from '../Page/Page';
+import AuthContext from '../../contexts/authContext';
+import { demoPagesMenu } from '../../menu';
+
+interface IPageWrapperProps {
+	isProtected?: boolean;
+	title?: string;
+	description?: string;
+	children:
+		| ReactElement<ISubHeaderProps>[]
+		| ReactElement<IPageProps>
+		| ReactElement<IPageProps>[];
+	className?: string;
+}
+const PageWrapper = forwardRef<HTMLDivElement, IPageWrapperProps>(
+	({ isProtected = true, title, description, className, children }, ref) => {
+		useLayoutEffect(() => {
+			// @ts-ignore
+			document.getElementsByTagName('TITLE')[0].text = `${title ? `${title} | ` : ''}${
+				import.meta.env.VITE_SITE_NAME
+			}`;
+			// @ts-ignore
+			document
+				?.querySelector('meta[name="description"]')
+				.setAttribute('content', description || import.meta.env.VITE_META_DESC || '');
+		});
+
+		const { user } = useContext(AuthContext);
+
+		const navigate = useNavigate();
+		useEffect(() => {
+			if (isProtected && user === '') {
+				navigate(`../${demoPagesMenu.login.path}`);
+			}
+			return () => {};
+			// eslint-disable-next-line react-hooks/exhaustive-deps
+		}, []);
+
+		return (
+			<div ref={ref} className={classNames('page-wrapper', 'container-fluid', className)}>
+				{children}
+			</div>
+		);
+	},
+);
+PageWrapper.displayName = 'PageWrapper';
+
+export default PageWrapper;
